0027df3aa699837001b48b6d441c5ea1658adbb2
Stefan <stefan.kaeser7@gmail.com>
2019-11-07 17:10:53 +0100

    [124] add instructions
    [124] add solution
    
    But I have to adjust my solution, because the official
    solution showed me, that I used Counter suboptimal.

---
+++ 124/marvel.py
@@ -0,0 +1,96 @@
+from collections import Counter, namedtuple
+import csv
+import re
+
+import requests
+
+MARVEL_CSV = 'https://raw.githubusercontent.com/pybites/marvel_challenge/master/marvel-wikia-data.csv'  # noqa E501
+
+Character = namedtuple('Character', 'pid name sid align sex appearances year')
+
+
+# csv parsing code provided so this Bite can focus on the parsing
+
+def _get_csv_data():
+    """Download the marvel csv data and return its decoded content"""
+    with requests.Session() as session:
+        return session.get(MARVEL_CSV).content.decode('utf-8')
+
+
+def load_data():
+    """Converts marvel.csv into a sequence of Character namedtuples
+       as defined above"""
+    content = _get_csv_data()
+    reader = csv.DictReader(content.splitlines(), delimiter=',')
+    for row in reader:
+        name = re.sub(r'(.*?)\(.*', r'\1', row['name']).strip()
+        yield Character(pid=row['page_id'],
+                        name=name,
+                        sid=row['ID'],
+                        align=row['ALIGN'],
+                        sex=row['SEX'],
+                        appearances=row['APPEARANCES'],
+                        year=row['Year'])
+
+characters = list(load_data())
+
+# start coding
+
+def most_popular_characters(characters=characters, top=5):
+    """Get the most popular character by number of appearances,
+       return top n characters (default 5)
+    """
+    def appearances_to_int(appearances):
+        try:
+            return int(appearances)
+        except ValueError:
+            return 0
+
+    name_appearances = [(character.name,
+                        appearances_to_int(character.appearances)) \
+                        for character in characters]
+    name_descending_appearances = sorted(name_appearances, reverse=True,
+                                            key=lambda x : x[1])
+    names, _ = zip(*name_descending_appearances)
+    # Test expects list
+    names = list(names)
+    return names[:top]
+
+from collections import Counter
+
+def max_and_min_years_new_characters(characters=characters):
+    """Get the year with most and least new characters introduced respectively,
+       use either the 'FIRST APPEARANCE' or 'Year' column in the csv
+       characters, or the 'year' attribute of the namedtuple, return a tuple
+       of (max_year, min_year)
+    """
+    year_number_new_characters = Counter()
+    for character in characters:
+        year_number_new_characters[character.year] +=1
+    # Delete unknown year
+    del year_number_new_characters['']
+    max_year = year_number_new_characters.most_common()[0][0]
+    min_year = year_number_new_characters.most_common()[-1][0]
+    return (max_year, min_year)
+
+
+def get_percentage_female_characters(characters=characters):
+    """Get the percentage of female characters as percentage of all genders
+       over all appearances.
+       Ignore characters that don't have gender ('sex' attribue) set
+       (in your characters data set you should only have Male, Female,
+       Agender and Genderfluid Characters.
+       Return the result rounded to 2 digits
+    """
+    gender_number_characters = Counter()
+    for character in characters:
+        gender_number_characters[character.sex] += 1
+
+    # Delete unknown gender
+    del gender_number_characters['']
+
+    number_female = gender_number_characters['Female Characters']
+    number_with_gender = sum(gender_number_characters.values())
+    percentage_female = 100 * number_female / number_with_gender
+
+    return round(percentage_female, 2)
