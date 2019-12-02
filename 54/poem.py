8289db617b9aee0ac5a69890d9abc269206e1b6d
Stefan <stefan.kaeser7@gmail.com>
2019-12-02 16:56:47 +0100

    [54] imporve solution
    Use dedent method of textwrap and also just break at double newline,
    i.e. new stanza insead of doing a index search.

--- 54/poem.py
+++ 54/poem.py
@@ -1,15 +1,13 @@
+import textwrap
+
+
 INDENTS = 4
 
 
 def print_hanging_indents(poem):
-    poem = poem.strip().split("\n")
-    poem = [line.strip() for line in poem]
-
-    emptyline_indices = [idx for idx, line in enumerate(poem) if not line]
-
-    formatted_string = ""
     seperater = "\n" + " " * INDENTS
-    for start, end in zip([-1] + emptyline_indices, emptyline_indices + [None]):
-        formatted_string += seperater.join(poem[start + 1 : end]) + "\n"
 
-    print(formatted_string[:-1])
+    # Use dedent to get rid of leading whitespace on every line
+    # Also break at double newline, i.e. new stanza
+    for stanza in textwrap.dedent(poem[1:]).split("\n\n"):
+        print(seperater.join(stanza.split("\n")))
