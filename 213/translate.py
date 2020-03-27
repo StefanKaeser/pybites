ccbe7c58e16f471279c9b9b70daf30491be40376
Stefan <stefan.kaeser7@gmail.com>
2020-03-27 23:17:33 +0100

    [213] refactor solution

--- 213/translate.py
+++ 213/translate.py
@@ -1,5 +1,6 @@
 import re
 
+
 def fix_translation(org_text, trans_text):
     """Receives original English text as well as text returned by translator.
        Parse trans_text restoring the original (English) code (wrapped inside
@@ -11,10 +12,7 @@ def fix_translation(org_text, trans_text):
     regexs = [inline_code_re, code_block_re]
 
     for regex in regexs:
-        originals = regex.findall(org_text)
-        translations = regex.findall(trans_text)
-        
-        for translation, original in zip(translations, originals): 
-            trans_text = trans_text.replace(translation, original)
+        originals = regex.finditer(org_text)
+        trans_text = regex.sub(lambda _ : next(originals).group(), trans_text)
 
     return trans_text
