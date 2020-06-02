17b76d3d409c8de3c07c1096b13c4e3d08d83ece
Stefan <stefan.kaeser7@gmail.com>
2020-06-02 17:04:07 +0200

    [272] add alternative solution

--- 272/strings.py
+++ 272/strings.py
@@ -10,13 +10,17 @@ def common_words(sentence1: List[str], sentence2: List[str]) -> List[str]:
             If there are duplicate words in the results, just choose one word. 
             Returned words should be sorted by word's length.
     """
-    common_words = set()
-    for word1 in sentence1:
-        word1 = word1.lower()
-        for word2 in sentence2:
-            word2 = word2.lower()
-            if word1 == word2:
-                common_words.add(word1)
+    #common_words = set()
+    #for word1 in sentence1:
+    #    word1 = word1.lower()
+    #    for word2 in sentence2:
+    #        word2 = word2.lower()
+    #        if word1 == word2:
+    #            common_words.add(word1)
+    sentence1 = set([word.lower() for word in sentence1])
+    sentence2 = set([word.lower() for word in sentence2])
+    common_words = sentence1 & sentence2
+
     return list(common_words)
 
 
