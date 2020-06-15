9f839af4ef400786b7c28701c2241f310bb4422c
Stefan <stefan.kaeser7@gmail.com>
2020-06-15 17:37:14 +0200

    [273] add improved version

--- 273/graph.py
+++ 273/graph.py
@@ -1,7 +1,7 @@
-#import heapq
 from collections import namedtuple
+from heapq import heappop, heappush
 
-Path = namedtuple("Path", ["vertices", "distance"])
+Path = namedtuple("Path", ["distance", "vertices"])
 
 
 def shortest_path(graph, start, end):
@@ -13,28 +13,24 @@ def shortest_path(graph, start, end):
        Output: tuple of (distance, [path of cites])
        Ex.   (distance, ['a', 'c', 'd', 'b])
     """
-    path = Path([start,], 0)
-    paths = [path,]
+    path = Path(0, [start,])
+    paths = [path]
 
-    step = 1
-    visited_vertices = [start,]
+    visited_vertices = [start]
     while True:
-        for path in paths:
-            vertices, distance = path
-            last_vertex = vertices[-1]
-            relative_distance = step - distance
+        path = heappop(paths)
 
-            connections = graph[last_vertex]
-            for next_vertex, needed_distance in connections.items():
-                not_already_visited = next_vertex not in visited_vertices 
-                possible_connection = needed_distance <= relative_distance
+        distance, vertices = path
+        last_vertex = vertices[-1]
 
-                if not_already_visited and possible_connection:
-                    new_path = Path(vertices + [next_vertex], step)
-                    paths.append(new_path)
+        if last_vertex == end:
+            return (path.distance, path.vertices)
 
-                    visited_vertices.append(next_vertex)
+        visited_vertices.append(last_vertex)
 
-                    if next_vertex == end:
-                        return (new_path.distance, new_path.vertices)
-        step += 1
+        connections = graph[last_vertex]
+        for next_vertex, needed_distance in connections.items():
+            not_already_visited = next_vertex not in visited_vertices
+            if not_already_visited:
+                new_path = Path(distance + needed_distance, vertices + [next_vertex])
+                heappush(paths, new_path)
